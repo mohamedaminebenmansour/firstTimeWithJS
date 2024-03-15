57ce25bf6c80e2b81bc96463394edffaf77ff8ea
mohamedaminebenmansour <mohamedploy9@gmail.com>
2024-03-15 02:12:33 +0100

    firstExemple

--- 01_HelloWorld.js
+++ 01_HelloWorld.js
@@ -1,1 +1,13 @@
 console.log("hello")
+let js = "amazing"
+if (js == "amazing") {
+    console.log("JavaScript is fun")
+}
+console.log("___-----_____------_____------_____")
+js = "boring"
+
+if (js == "amazing") {
+    console.log("JavaScript is fun")
+} else {
+    console.log("JavaScript is boring")
+}
