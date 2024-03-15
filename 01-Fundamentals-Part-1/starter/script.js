61cc72d1ca6898fc800cca0b452c7b73119d668a
mohamedaminebenmansour <mohamedploy9@gmail.com>
2024-03-15 04:03:29 +0100

    Values and variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -2,4 +2,28 @@ let js = "amazing"
 if (js == "amazing") {
     console.log("JavaScript is fun")
     alert("JavaScript is fun")
-}
+};
+console.log(40 + 30 - 20)
+//let's talk about values
+/*value is basicaly a piece of data So it's the moost fundamental unit
+of information that we have in programming*/
+console.log("mohamed");//mohamed is a value
+console.log(30);//30 is a value
+//let's talk about variables
+/*Now, one extremely useful thing we can do with values is to store them 
+into variables and so this way we can reuse them over and over again */
+let firstName = "mohamed";
+/*so what we did here is called decalaring a variable and so this actually
+create a real variable in my computer memory and we'll store this value
+"mohamed" inside of this variable "firstName"*/
+console.log(firstName)
+let camelCase = "mohamed"; //use camelCase
+//let 3years=3; //illegal variable name because starts with number
+/*And in fact names can only contains numbers ,letters, unerscores(_) or
+the dollar assign ($) */
+//let med&amin="mohamed amin"; // illegl because we use "&"
+//let new = "mohamed;" //illegal name a variable using a reserved JS keyword
+let Person = "mohamed";//works but aanother convention we should not start a variable name with an upperCase
+let PI = 3.14;/* this number is never gonna change 
+and so that's a constant and for that, we have a conventin of writing it
+in all upper case*/
