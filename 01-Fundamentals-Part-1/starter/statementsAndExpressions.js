f58d91df667ed485536e5c9ed5e6074467adf775
mohamedaminebenmansour <mohamedploy9@gmail.com>
2024-03-18 04:45:15 +0100

    Statements and expressions

---
+++ 01-Fundamentals-Part-1/starter/statementsAndExpressions.js
@@ -0,0 +1,11 @@
+// Statements and Expressions
+3 + 4 //expression
+1991 //expression
+true && false && !false //expression
+
+if (23 > 10) {
+    const str = '23 is bigger';//declaring this variable not an action dosent produce a value
+}
+
+const me = 'mohamed';
+console.log(`I'm ${2024 - 1993} years old ${me}`);
