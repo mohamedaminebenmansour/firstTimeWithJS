6bd6c234816599d5ef6a7eefaa9a3b11994023f8
mohamedaminebenmansour <mohamedploy9@gmail.com>
2024-03-18 04:55:38 +0100

    the conditional (trenary) operator

---
+++ 01-Fundamentals-Part-1/starter/theConditinalTrenary.js
@@ -0,0 +1,21 @@
+// The Conditional (Ternary) Operator
+const age = 23;
+// age >= 18 ? console.log('I like to drink wine 🍷') : console.log('I like to drink water 💧');
+
+const drink = age >= 18 ? 'wine 🍷' : 'water 💧';
+/*we have 3 part
+    1)condition part: age >= 18
+    2)if part:        'wine 🍷'
+    3)else part:      'water 💧'
+ */
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+    drink2 = 'wine 🍷';
+} else {
+    drink2 = 'water 💧';
+}
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? 'wine 🍷' : 'water 💧'}`);
