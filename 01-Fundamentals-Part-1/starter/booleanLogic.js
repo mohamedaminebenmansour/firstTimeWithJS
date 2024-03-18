ba8fe0f4435034b936de1d399f0ae292838e249c
mohamedaminebenmansour <mohamedploy9@gmail.com>
2024-03-18 03:56:53 +0100

    Bolean logic

---
+++ 01-Fundamentals-Part-1/starter/booleanLogic.js
@@ -0,0 +1,12 @@
+console.log("true && true=", true && true);
+console.log("true && false=", true && false);
+console.log("false && true=", false && true);
+console.log("false && false=", false && false);
+
+console.log("true || true=", true || true);
+console.log("true || false=", true || false);
+console.log("false || true=", false || true);
+console.log("false || false=", false || false);
+
+console.log("!true=", !true);
+console.log("!false=", !false);
