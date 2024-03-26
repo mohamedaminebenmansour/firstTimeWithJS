59e76665e38261e587d6ec85d23b14e1a0e79009
mohamedaminebenmansour <mohamedploy9@gmail.com>
2024-03-26 01:26:50 +0100

    Handling Click Events

--- 04-GuessMyNumber/script.js
+++ 04-GuessMyNumber/script.js
@@ -1,4 +1,5 @@
 'use strict';
+/*
 console.log(document.querySelector('.message').textContent);
 // .message if message is class 
 //#message if message is id
@@ -9,3 +10,12 @@ document.querySelector('.number').textContent = 99;
 document.querySelector('.score').textContent = 100;
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);//get value from
+*/
+document.querySelector('.check').addEventListener('click', function (e) {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+    if (!guess) {
+        document.querySelector('.message').textContent = 'No Number!!';
+    }
+
+})
